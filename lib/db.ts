f443cd6b4a30f87e33027106efa0ba9b9c66caf0
Lucas Souza <lsouza.ls404@gmail.com>
2026-03-05 19:41:55 -0300

    Fix db pool for optional DATABASE_URL

--- lib/db.ts
+++ lib/db.ts
@@ -2,20 +2,21 @@ import { Pool } from "pg";
 import { env } from "./env";
 
 declare global {
-  // eslint-disable-next-line no-var
   var __pgPool: Pool | undefined;
 }
 
 export const pool =
-  global.__pgPool ??
-  new Pool({
-    connectionString: env.databaseUrl,
-    ssl:
-      process.env.NODE_ENV === "production"
-        ? { rejectUnauthorized: false }
-        : false,
-  });
+  env.databaseUrl
+    ? global.__pgPool ??
+      new Pool({
+        connectionString: env.databaseUrl,
+        ssl:
+          process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
+      })
+    : null;
 
-if (process.env.NODE_ENV !== "production") {
+if (env.databaseUrl && !global.__pgPool && pool) {
   global.__pgPool = pool;
 }
